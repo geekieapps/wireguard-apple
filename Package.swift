af1667a453fa66a70c4fd5e5b514119c81f13c8f
geekieapps <geekieapps@gmail.com>
2026-04-11 18:12:11 +0300

    feature: expose wireguardkitc as a product

--- Package.swift
+++ Package.swift
@@ -10,7 +10,8 @@ let package = Package(
         .iOS(.v15)
     ],
     products: [
-        .library(name: "WireGuardKit", targets: ["WireGuardKit"])
+        .library(name: "WireGuardKit", targets: ["WireGuardKit"]),
+        .library(name: "WireGuardKitC", targets: ["WireGuardKitC"]),
     ],
     dependencies: [],
     targets: [
